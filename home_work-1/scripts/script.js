b305a49424a6f81c9cf3bda8cca6d41aca156cdf
kozachuk-ivan <kozachuk161@gmail.com>
2022-08-05 13:46:01 +0300

    optimization of script

--- home_work-1/scripts/script.js
+++ home_work-1/scripts/script.js
@@ -1,74 +1,37 @@
-const giveFunctionsToButtons = {
-	choseFirstAndLastElementCycle(arr) {
-		for (let i = 0; i < arr.length; i++) {
-			if (arr[i].hasAttribute('style')) {
-				arr[i].removeAttribute('style');
+function choseFirstOrLastElement(firstOrLast) {
+	let parent = document.getElementById("list");
+	firstOrLast === 'first' ? firstElement = parent.firstElementChild : lastElement = parent.lastElementChild;
+	for(let i = 0; i < parent.children.length; i++) {
+		if (parent.children[i].hasAttribute('style')) parent.children[i].removeAttribute('style');
+	}
+	(firstOrLast === 'first') ? firstElement.setAttribute('style', 'color:red') : lastElement.setAttribute('style', 'color:red');
+}
+function choseNextOrPrevElement(nextOrPrev) {
+	let parent = document.getElementById("list");
+	let activeChild;
+	let positionOfChildrens;
+	for (let i = 0; i < parent.children.length; i++) {
+		if(parent.children[i].hasAttribute('style')) {
+			activeChild = parent.children[i];
+			if (activeChild[nextOrPrev === 'next' ? 'nextElementSibling' : 'previousElementSibling'] === null) {
+				(nextOrPrev === 'next') ? parent.children[0].setAttribute('style', 'color:red') : parent['lastElementChild'].setAttribute('style', 'color:red');
+				parent[nextOrPrev === 'next' ? 'lastElementChild' : 'firstElementChild'].removeAttribute('style');
 			}
+			positionOfChildrens = activeChild[nextOrPrev === 'next' ? 'nextElementSibling' : 'previousElementSibling'];
+			positionOfChildrens.setAttribute('style', 'color:red');
+			positionOfChildrens[nextOrPrev === 'next' ? 'previousElementSibling' : 'nextElementSibling'].removeAttribute('style');
+			break;
 		}
-	},
-	choseFirstElement() {
-		let parent = document.getElementById("list");
-		firstElement = parent.firstElementChild;
-		this.choseFirstAndLastElementCycle(parent.children);
-		firstElement.style.color = 'red';
-	},
-	choseLastElement() {
-		let parent = document.getElementById("list");
-		lastElement = parent.lastElementChild;
-		this.choseFirstAndLastElementCycle(parent.children);
-		lastElement.style.color = 'red';
-	},
-	choseNextElement() {
-		let parent = document.getElementById("list");
-		let activeChild;
-		let nextChild;
-		for (let i = 0; i < parent.children.length; i++) {
-			if(parent.children[i].hasAttribute('style')) {
-				activeChild = parent.children[i];
-				if (activeChild.nextElementSibling === null) {
-					parent.children[0].style.color = 'red';
-					parent.lastElementChild.removeAttribute('style');
-				}
-				nextChild = activeChild.nextElementSibling;
-				nextChild.style.color = 'red';
-				nextChild.previousElementSibling.removeAttribute('style');
-				break;
-			}
-		}
-	},
-	chosePrevElement() {
-		let parent = document.getElementById("list");
-		let activeChild;
-		let prevChild;
-		for (let i = 0; i < parent.children.length; i++) {
-			if(parent.children[i].hasAttribute('style')) {
-				activeChild = parent.children[i];
-				if (activeChild.previousElementSibling === null) {
-					parent.lastElementChild.style.color = 'red';
-					parent.firstElementChild.removeAttribute('style');
-				}
-				prevChild = activeChild.previousElementSibling;
-				prevChild.style.color = 'red';
-				prevChild.nextElementSibling.removeAttribute('style');
-				break;
-			}
-		}
-	},
-	addElement() {
-		let createTag = document.createElement('li');
-		createTag.innerHTML = 'Уже этот элемент последний';
-		let parent = document.getElementById('list').appendChild(createTag);
-		parent.previousElementSibling.innerHTML = 'Элемент списка';
-	}, 
-	deleteElement() {
+	}
+}
+function deleteElement() {
 		let parent = document.getElementById('list');
 		let remove = parent.lastElementChild;
 		parent.removeChild(remove);
-	}, 
-	AddElementInBeggin() {
-		let element = document.createElement('li');
-		element.innerHTML = 'А я первый))';
-		let parent = document.getElementById('list');
-		parent.insertBefore(element, parent.children[0]);
-	}, 
-};
+}
+function addElementsInBegginOrEnd(begginOrEnd) {
+	let element = document.createElement('li');
+	(begginOrEnd === 'beggin') ? element.textContent = 'А я первый)))' : element.textContent = 'Уже я последний(((';
+	let parent = document.getElementById('list');
+	(begginOrEnd === 'beggin') ? parent.insertBefore(element, parent.children[0]) : parent.appendChild(element);
+}
